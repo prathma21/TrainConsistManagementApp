aa3498547463bfabac82356bbf00c1d90022f7a5
Your GitHub Username <prathma0801@gmail.com>
2026-04-10 14:22:29 +0530

    prathma21 Added UC1-InitializeTrainandDisplayConsistSummary

---
+++ App/src/TrainConsistManagementApp.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class TrainConsistManagementApp {
+    public static void main(String[] args){
+        //DISPLAY WELCOME BANNER
+        System.out.println("========================================");
+        System.out.println("   === Train Consist Management App === ");
+        System.out.println("========================================\n");
+
+        //Create a dynamic list to store train bagies
+        List<String> trainConsist = new ArrayList<>();
+
+        //Display initial consist information
+        System.out.println("Train initialized successfully...");
+    }
+}
